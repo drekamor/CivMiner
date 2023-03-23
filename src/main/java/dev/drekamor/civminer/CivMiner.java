c9597d3ee5abe9aa3b6a5fd63323a019f19612c8
Drekamor <ddrekamor@gmail.com>
2023-03-23 23:56:17 +0000

    fix errors

--- src/main/java/dev/drekamor/civminer/CivMiner.java
+++ src/main/java/dev/drekamor/civminer/CivMiner.java
@@ -1,14 +1,14 @@
 package dev.drekamor.civminer;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CivMiner implements ModInitializer {
+public class CivMiner implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("civ-miner");
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 
 		LOGGER.info("Hello Fabric world!");
 	}
